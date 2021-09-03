d93659148cf39712df81b15cab8de291b47f76d5
Babatunde Adenowo <mo.xodeeq@gmail.com>
2021-09-03 03:03:46 +0100

    Made responsive

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import ShiftCard from "./ShiftCard";
 
 function App() {
   return (
-    <main className="App bg-gray-100 p-4 lg:px-40 space-y-6 text-xs">
+    <main className="App bg-gray-100 p-4 lg:px-10 xl:px-16 space-y-6 text-xs md:text-sm lg:text-base">
       <header className="flex justify-between">
-        <button className="flex items-center text-center justify-between w-28 bg-white border-2 border-green-400 px-2 py-1 rounded-md">
+        <button className="flex items-center text-center justify-between lg:justify-around w-28 lg:w-40 bg-white border-2 border-green-400 px-2 py-1 lg:py-2 rounded-md">
           <p className="font-medium">Tomorrow</p>
           <ChevronDownIcon className="w-6 h-6" />
         </button>
-        <button className="bg-green-500 w-28 text-white px-2 py-1 rounded-md font-medium">
+        <button className="bg-green-500 w-28 lg:w-40 text-white px-2 py-1 rounded-md font-medium">
           Add new shift
         </button>
       </header>
       <section className="space-y-6">
-        <header className="w-full flex items-center text-left justify-between font-medium px-6 py-4 bg-green-500 text-white rounded-md">
+        <header className="w-full flex items-center xl:items-start text-left justify-between xl:pl-20 font-medium px-6 py-4 bg-green-500 text-white rounded-md">
           <h3 className="">Staff Name</h3>
           <h3 className="hidden lg:inline-block">Lead</h3>
           <h3 className="hidden lg:inline-block">Title</h3>
-          <h3 className="text-right">Shift Time</h3>
+          <h3 className="text-right xl:text-left">Shift Time</h3>
+          <div className="hidden lg:inline-block"></div>
+          <div className="hidden lg:inline-block"></div>
         </header>
 
         <div className="space-y-8">
--- src/ShiftCard.js
+++ src/ShiftCard.js
@@ -1,8 +1,8 @@
 function ShiftCard() {
   return (
-    <div className="bg-white space-y-5 p-5 rounded-2xl">
-      <div className="flex justify-between">
-        <div className="flex space-x-3 align-middle items-center">
+    <div className="xl:flex xl:justify-between bg-white space-y-5 p-5 rounded-2xl">
+      <div className="flex justify-between xl:space-x-28">
+        <div className="flex space-x-3 xl:space-x-6 align-middle items-center">
           <img
             src="https://res.cloudinary.com/moh-x/image/upload/v1618542907/fcc/portfolio/AvatarMaker_ipxz90.svg"
             alt="user's avatar"
@@ -10,15 +10,25 @@ function ShiftCard() {
           />
           <span>Babatunde Adenowo</span>
         </div>
-        <div>09:00am - 12:00pm</div>
+
+        <p className="hidden my-auto lg:block text-left text-gray-500 items-center">
+          Developer
+        </p>
+
+        <p className="hidden my-auto lg:block text-left text-gray-500 items-center">
+          Eyimofe Omotayo
+        </p>
+
+        <div className="my-auto">09:00am - 12:00pm</div>
       </div>
-      <div className="flex justify-between items-center">
-        <div className="">
-          <button className="flex justify-between text-center w-28 bg-white border-2 border-green-400 py-2 rounded-md">
+
+      <div className="flex justify-between items-center xl:space-x-12">
+        <div className="my-auto">
+          <button className="flex my-auto justify-between text-center w-28 bg-white border-2 border-green-400 py-2 rounded-md">
             <p className="font-medium text-green-500 mx-auto">Edit Shift</p>
           </button>
         </div>
-        <div className="text-red-500">Delete Shift</div>
+        <p className="text-red-500 my-auto">Delete Shift</p>
       </div>
     </div>
   );
